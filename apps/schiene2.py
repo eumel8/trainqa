d91e57dbd78b55ea7a10050a988000efe3e8961c
Frank Kloeker <f.kloeker@telekom.de>
2020-01-13 08:11:34 +0100

    add infos about missing train parts

--- apps/schiene2.py
+++ apps/schiene2.py
@@ -9,8 +9,8 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 homestation = '8013472'
 
-def gspread_data(date,trainnumber,traindeparture,traintarget,traindelay,traintext):
-    row = [date,trainnumber,traindeparture,traintarget,traindelay,traintext]
+def gspread_data(date,trainnumber,traindeparture,traintarget,traindelay,traintext,trainnote):
+    row = [date,trainnumber,traindeparture,traintarget,traindelay,traintext,trainnote]
     scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
     creds = ServiceAccountCredentials.from_json_keyfile_name('gspread-secret.json', scope)
     client = gspread.authorize(creds)
@@ -44,15 +44,13 @@ def parse_departures1(html,dt):
     soup = BeautifulSoup(html, "html.parser")
     date = dt.strftime("%d.%m.%y")
 
-    # print ("Date\t\tTrain\t\tDeparture\tTarget\t\t\t\tDelay\tText")
     for row in soup.find_all('div', attrs={'class': 'sqdetailsDep trow'}):
         trainnumber = row.span.contents[0]
         trainurl = row.a['href']
         traintarget = row.br.previous_element.strip()[3:]
         traindeparture = (row.find_all('span')[-1].get_text())
-        traindelay, traintext = traindetails(date,trainnumber,trainurl)
-        gspread_data(date,trainnumber,traindeparture,traintarget,traindelay,traintext)
-        #print ("%s\t%s\t%s\t\t%s\t\t\t\t%s\t%s" % (date,trainnumber,traindeparture,traintarget,traindelay,traintext))
+        traindelay, traintext, trainnote = traindetails(date,trainnumber,trainurl)
+        gspread_data(date,trainnumber,traindeparture,traintarget,traindelay,traintext,trainnote)
     return
 
 def parse_train(html):
@@ -60,21 +58,25 @@ def parse_train(html):
     soup = BeautifulSoup(html, "html.parser")
     traindelay = '0'
     traintext = ''
+    trainnote = ''
 
     for row in soup.find_all("st"):
         station = row.get('evaid')
         if station == homestation:
           traindelay = row.get('arrdelay')
           traintext = row.get('freitext')
+    for row in soup.find_all("himmessage", attrs={'display': 'QFT'}):
+    #print (soup.find("himmessage",display='QFT').header)
+        trainnote = row.get('header')
 
-    return traindelay,traintext
+    return traindelay,traintext,trainnote
 
 def traindetails(date,trainnumber,trainurl):
 
     trainurl = trainurl.replace('dox','dn')
     rsp = requests.get(trainurl)
-    traindelay, traintext = parse_train(rsp.text)
-    return traindelay,traintext
+    traindelay, traintext, trainnote = parse_train(rsp.text)
+    return traindelay,traintext, trainnote
 
 def departures(origin, dt=datetime.now()):
 
